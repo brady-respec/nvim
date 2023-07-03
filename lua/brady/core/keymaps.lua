e9c392c89cbdb9e0adc8ac36930be0bf63d16597
brady-respec <brady.mills@respec.com>
2023-07-03 15:51:14 -0600

    Started migrating to a multi-file setup.

---
+++ lua/brady/core/keymaps.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+
+-- Bring up file explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Set keymaps to navigate windows
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Set keymap to close windows
+vim.keymap.set('n', '<leader>x', ":q<CR>")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Remap to make it so we only scroll half pages
+vim.keymap.set({'n', 'v'}, '<C-b>', '<C-u>')
+vim.keymap.set({'n', 'v'}, '<C-f>', '<C-d>')
+
